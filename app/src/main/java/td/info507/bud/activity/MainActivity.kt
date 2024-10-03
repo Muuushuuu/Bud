239c2bd5a0401283b45e81edeaf6a7001b541c0b
Antoine <104618144+AntoineIV@users.noreply.github.com>
2024-10-03 11:39:12 +0200

    xml activity main

--- app/src/main/java/td/info507/bud/activity/MainActivity.kt
+++ app/src/main/java/td/info507/bud/activity/MainActivity.kt
@@ -1,13 +1,13 @@
-package td.info507.bud
+package td.info507.bud.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import td.info507.bud.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
-// fiuzeguivgauoie
